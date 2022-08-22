877d070adbef40fc47aa082bf998bee10030fd96
mehm8128 <mehm8128@gmail.com>
2022-08-22 22:49:50 +0900

    fix layout

--- src/components/container/GoalListContainer.tsx
+++ src/components/container/GoalListContainer.tsx
@@ -3,7 +3,7 @@ import GoalSettingForm from "../goal/GoalSettingForm"
 
 const GoalListContainer: React.FC = () => {
 	return (
-		<div className="flex flex-col-reverse justify-around p-4 md:flex-row">
+		<div className="flex flex-col-reverse justify-around gap-12 p-4 md:flex-row">
 			<GoalList />
 			<div className="md:w-3/10 h-4/5 w-full border-2 p-8">
 				<h2>目標の設定</h2>
