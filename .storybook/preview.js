57353c88e3277da21ba8b8f04ded95d2cb913098
mehm8128 <mehm8128@gmail.com>
2022-11-27 18:40:08 +0900

    add next router addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,9 @@
 import "windi.css"
 import "antd/dist/antd.css"
 import "styles/globals.css"
+import { RouterContext } from "next/dist/shared/lib/router-context"
+import { RecoilRoot } from "recoil"
+import { withNextRouter } from "storybook-addon-next-router"
 
 export const parameters = {
 	actions: { argTypesRegex: "^on[A-Z].*" },
@@ -10,4 +13,25 @@ export const parameters = {
 			date: /Date$/,
 		},
 	},
+	nextRouter: {
+		Provider: RouterContext.Provider,
+		path: "/",
+		asPath: "/",
+		query: {},
+		push() {},
+	},
 }
+
+export const decorators = [
+	(Story) => (
+		<RecoilRoot>
+			<Story />
+		</RecoilRoot>
+	),
+	// withNextRouter({
+	// 	path: "/",
+	// 	asPath: "/",
+	// 	query: {},
+	// 	push() {},
+	// }),
+]
